ad93abd74c7d393c9407fd7d10ca1af2ca3d9c94
Felipe Ferreira <mfelipeof@gmail.com>
2021-05-06 14:35:56 +0100

    feat(cpp-code): Updated to unreal 4.26 + lesson 49
    Coding The Basic Game Loop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,25 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bull and Cows game!!"));
+    PrintLine(TEXT("Guess the 4 characters word"));
+    PrintLine(TEXT("Press [ENTER] key"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
 
+    FString HiddenWord = TEXT("cake");
+
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You found it!"));
+        PrintLine(HiddenWord);
+    } 
+    else 
+    {
+        PrintLine(TEXT("You LOST"));
+    }
+    
 }
